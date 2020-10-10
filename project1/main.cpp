e7c2d7b20af7e4dee91f509772082f36bb221f19
“Hanna20001210” <hanna70343@gmail.com>
2020-10-11 00:13:11 +0800

    std

---
+++ project1/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  project1
+//
+//  Created by Hanna on 2020/10/10.
+//  Copyright © 2020 鄭佳渝. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
