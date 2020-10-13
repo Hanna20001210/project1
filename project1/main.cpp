53b5384eca0e82c4b3584c0f3ba505b21ca1961b
“Hanna20001210” <hanna70343@gmail.com>
2020-10-13 10:57:04 +0800

    blocks setting, matrix setting, basic functions

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,16 +1,160 @@
-//
-//  main.cpp
-//  project1
-//
-//  Created by Hanna on 2020/10/10.
-//  Copyright © 2020 鄭佳渝. All rights reserved.
-//
 
 #include <iostream>
+int **matrix;
+int matrix_width, matric_height,ref,n,id,right_limit,up_limit,row;
+
+int blocks[19][4][4]{
+    {{0,1,0,0},{1,1,1,0},{0,0,0,0},{0,0,0,0}},//T1
+    {{0,1,0,0},{1,1,0,0},{0,1,0,0},{0,0,0,0}},//T2
+    {{1,1,1,0},{0,1,0,0},{0,0,0,0},{0,0,0,0}},//T3
+    {{1,0,0,0},{1,1,0,0},{1,0,0,0},{0,0,0,0}},//T4
+    {{1,1,0,0},{1,0,0,0},{1,0,0,0},{0,0,0,0}},//L1
+    {{1,0,0,0},{1,1,1,0},{0,0,0,0},{0,0,0,0}},//L2
+    {{0,1,0,0},{0,1,0,0},{1,1,0,0},{0,0,0,0}},//L3
+    {{1,1,1,0},{0,0,1,0},{0,0,0,0},{0,0,0,0}},//L4
+    {{1,1,0,0},{0,1,0,0},{0,1,0,0},{0,0,0,0}},//J1
+    {{1,1,1,0},{1,0,0,0},{0,0,0,0},{0,0,0,0}},//J2
+    {{1,0,0,0},{1,0,0,0},{1,1,0,0},{0,0,0,0}},//J3
+    {{0,0,1,0},{1,1,1,0},{0,0,0,0},{0,0,0,0}},//J4
+    {{1,1,0,0},{0,1,1,0},{0,0,0,0},{0,0,0,0}},//S1
+    {{0,1,0,0},{1,1,0,0},{1,0,0,0},{0,0,0,0}},//S2
+    {{0,1,1,0},{1,1,0,0},{0,0,0,0},{0,0,0,0}},//Z1
+    {{1,0,0,0},{1,1,0,0},{0,1,0,0},{0,0,0,0}},//Z2
+    {{1,0,0,0},{1,0,0,0},{1,0,0,0},{1,0,0,0}},//I1
+    {{1,1,1,1},{0,0,0,0},{0,0,0,0},{0,0,0,0}},//I2
+    {{1,1,0,0},{1,1,0,0},{0,0,0,0},{0,0,0,0}},//O
+};
 
 using namespace std;
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    cout << "Hello, World!\n";
+int main(void) {
+    
+    cin >> matrix_width >> matric_height;
+
+    for (int i = 1; i <= matric_height; i++)
+    {
+        matrix[i] = new int[matrix_width];
+    }
+    
+    string name;
+    cin>>name>>ref>>n;
+    
+    Define_id(name);
+    
+    for (int i = 1; i <= matric_height; i++)
+    {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
     return 0;
+}
+
+bool Left_Boundary(){
+    if( ref < 0){
+        return false;
+    }
+    for(int j = 0; j <= up_limit; j++){
+        if(matrix[row+j][ref-1] != 0 || block[id][j][0]){
+            
+        }
+    }
+}
+
+void Right_Boundary(){
+    
+}
+
+void Clear(){
+    bool clear = true;
+    for (int i = 0; i < matric_height ; i++){
+        for (int j = matrix_width - 1; j >= 0; j--){
+            if(matrix[i][j] != 0){
+                clear = false;
+                break;
+            }
+        }
+        if(clear == true){
+            for (int m = 0; m < matrix_width; m++){
+                matrix[m][i]=0;
+            }
+        }
+    }
+}
+
+void Define_id(string a){
+    if(a == "T1"){
+        id = 0;
+        right_limit = 2;
+    }
+    else if ( a == "T2"){
+        id = 1;
+        right_limit = 1;
+    }
+    else if ( a == "T3"){
+        id = 2;
+        right_limit = 2;
+    }
+    else if ( a == "T4"){
+        id = 3;
+        right_limit = 1;
+    }
+    else if ( a == "L1"){
+        id = 4;
+        right_limit = 1;
+    }
+    else if ( a == "L2"){
+        id = 5;
+        right_limit = 2;
+    }
+    else if ( a == "L3"){
+        id = 6;
+        right_limit = 1;
+    }
+    else if ( a == "L4"){
+        id = 7;
+        right_limit = 2;
+    }
+    else if ( a == "J1"){
+        id = 8;
+        right_limit = 1;
+    }
+    else if ( a == "J2"){
+        id = 9;
+        right_limit = 2;
+    }
+    else if ( a == "J3"){
+        id = 10;
+        right_limit = 1;
+    }
+    else if ( a == "J4"){
+        id = 11;
+        right_limit = 2;
+    }
+    else if ( a == "S1"){
+        id = 12;
+        right_limit = 2;
+    }
+    else if ( a == "S2"){
+        id = 13;
+        right_limit = 1;
+    }
+    else if ( a == "Z1"){
+        id = 14;
+        right_limit = 2;
+    }
+    else if ( a == "Z2"){
+        id = 15;
+        right_limit = 1;
+    }
+    else if ( a == "I1"){
+        id = 16;
+        right_limit = 0;
+    }
+    else if ( a == "I2"){
+        id = 17;
+        right_limit = 3;
+    }
+    else if ( a == "O"){
+        id = 18;
+        right_limit = 1;
+    }
 }
